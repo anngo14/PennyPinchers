7c59cbf9d87f0698e0b0d82875f199da254ec4a7
Andrew <aandrewnngo@gmail.com>
2020-02-08 04:33:01 -0600

    home initial view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,24 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatTabsModule} from '@angular/material/tabs';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -30,6 +39,11 @@ import { RegisterComponent } from './register/register.component';
     MatFormFieldModule,
     MatMenuModule,
     MatSidenavModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatIconModule,
+    MatDividerModule,
+    MatTabsModule,
     BrowserAnimationsModule
   ],
   providers: [],
